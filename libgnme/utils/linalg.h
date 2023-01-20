b090c254f7ddd4e12450a92358869e7b389f1bee
Hugh Burton <hb407@cam.ac.uk>
2023-01-20 10:20:06 +0000

    Added adjoint matrix determinant update, implemented for wick_rscf

--- libgnme/utils/linalg.h
+++ libgnme/utils/linalg.h
@@ -31,6 +31,17 @@ void gen_eig_sym(
     const size_t dim, arma::Mat<T> &M, arma::Mat<T> &S, arma::Mat<T> &X, 
     arma::Col<double> &eigval, arma::Mat<T> &eigvec, double thresh=1e-8);
 
+
+/** \brief Compute the adjoint of a matrix
+    \param M Input matrix of interest
+    \param A Adjoint matrix of M
+    \param D Value of the determinant
+ **/
+template<typename T>
+void adjoint_matrix(arma::Mat<T> &M, arma::Mat<T> &A, T &det, size_t &nzero, double thresh=1e-16);
+
+
+
 } // namespace libgnme
 
 #endif // LIBGNME_LINALG_H_
